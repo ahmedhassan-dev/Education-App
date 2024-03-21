5c704bd18bd69f958a908753f483997595b25f50
ahmedhassan-dev <ahmedhassan.op9@gmail.com>
2024-03-21 11:23:33 +0200

    Add conditional logic for web platform in auth and main files
    - Import 'kIsWeb' from flutter/foundation and use it to conditionally execute
      code based on the platform being web.
    - Add a check in AuthCubit to only fetch and send token if not running on web.
    - Update Firebase initialization in main_development.dart and main_production.dart
      to use DefaultFirebaseOptions.web for web platform.
    - Add a check in MyApp to subscribe to 'admin' topic only if not running on web.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 String documentIdFromLocalData() => DateTime.now().toIso8601String();
 String currentTeacherVersion = "1.0.0";
 String currentStudentVersion = "1.0.0";
+
+abstract class AuthManager {
+  static bool isWeb = kIsWeb;
+}
